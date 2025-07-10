35ab0f12ebec29d8bccba616803e18b939f4c5ac
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-07-10 07:58:31 -0500

    :building_construction: WIP: ArrayViz `Makefile` + API
    - Added new target to `Makefile` that represents the test for array
      visualization and manual array arena testing
    - Started the `array_viz.h` API that is what any of the servers expect
      to be available from the respective data structure
    - `touch`'d a `test_vector_static_alloca.c` file, which will be the host
      server for the manual array arena testing.

---
+++ test/array_viz.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "arena-viz-msg.pb.c.h"
+
+size_t ArrayViz_Get_ArenaLayout( struct ArrayViz_List *, size_t max_len );
+size_t ArrayViz_Get_ArenaSize(void);
+
