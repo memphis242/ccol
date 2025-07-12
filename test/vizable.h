0bc6205a051511ebd867ffae9b15ec26cef13168
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-07-12 17:37:27 -0500

    :pick: `vizable` decouple from protobuf msg datatypes

--- test/vizable.h
+++ test/vizable.h
@@ -1,15 +1,25 @@
 #ifndef VIZABLE_TRAIT_H
 #define VIZABLE_TRAIT_H
 
-#include <stdint.h>
 #include <stdbool.h>
 
-#include "viz-msgs.pb.c.h"
+struct ArenaVizBlk
+{
+   size_t blk_offset;
+   size_t blk_len;
+};
+
+struct ArenaVizList
+{
+   struct ArenaVizBlk * list;
+   size_t len;
+};
 
 struct Vizable
 {
-   size_t (*GetArenaLayout)( struct ArenaVizList *, size_t );
-   size_t (*GetArenaSize)(void);
+   size_t (*ArenaLayout)(struct ArenaVizList *, size_t);
+   size_t (*ArenaSize)(void);
 };
 
 #endif // VIZABLE_TRAIT_H 
+
